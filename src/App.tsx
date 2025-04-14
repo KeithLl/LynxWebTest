6c416a3e08f813ecdd040f401df3bf99f52562a6
lile <lile@knowbox.cn>
2025-04-14 10:26:01 +0800

    main thread

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,73 @@
-import { useCallback, useEffect, useState } from '@lynx-js/react'
+import {
+  runOnBackground,
+  useCallback,
+  useEffect,
+  useLynxGlobalEventListener,
+  useMainThreadRef,
+  useState
+} from "@lynx-js/react";
 
-import './App.css'
-import arrow from './assets/arrow.png'
-import lynxLogo from './assets/lynx-logo.png'
-import reactLynxLogo from './assets/react-logo.png'
+import "./App.css";
+import "./index.css";
+import arrow from "./assets/arrow.png";
+import lynxLogo from "./assets/lynx-logo.png";
+import reactLynxLogo from "./assets/react-logo.png";
+import { MainThread } from "@lynx-js/types";
 
 export function App() {
-  const [alterLogo, setAlterLogo] = useState(false)
+  const [alterLogo, setAlterLogo] = useState(false);
+  const [data, setData] = useState(String);
+  const logoRef = useMainThreadRef<MainThread.Element>(null);
 
   useEffect(() => {
-    console.info('Hello, ReactLynx')
-  }, [])
+    console.info("Hello, ReactLynx");
+  }, []);
+
+  const handleTap = () => {
+    "main thread";
+    runOnBackground(setAlterLogo)(!alterLogo);
+  };
 
   const onTap = useCallback(() => {
-    'background only'
-    setAlterLogo(!alterLogo)
-  }, [alterLogo])
+    "main thread";
+    runOnBackground(setAlterLogo)(!alterLogo);
+
+    console.log("logoRef : " + logoRef.current);
+  }, [alterLogo]);
+
+  useLynxGlobalEventListener("testEvent", (data) => {
+    console.log("testEvent : " + data);
+    setAlterLogo(!alterLogo);
+  });
 
   return (
     <view>
-      <view className='Background' />
-      <view className='App'>
-        <view className='Banner'>
-          <view className='Logo' bindtap={onTap}>
+      <view className="Background"/>
+      <view className="App">
+        <view className="Banner">
+          <view className="Logo" main-thread:bindtap={onTap}
+                main-thread:ref={logoRef}>
             {alterLogo
-              ? <image src={reactLynxLogo} className='Logo--react' />
-              : <image src={lynxLogo} className='Logo--lynx' />}
+              ? <image src={reactLynxLogo} className="Logo--react"/>
+              : <image src={lynxLogo} className="Logo--lynx"/>}
           </view>
-          <text className='Title'>React</text>
-          <text className='Subtitle'>on Lynx</text>
+          {alterLogo ? <text className="Title">Test Msg</text> :
+            <text className="Title">MsgHaha</text>}
+          <input className="myInput" type={"text"} />
+          <text className="Title">React</text>
+          <text className="Subtitle">on Lynx</text>
         </view>
-        <view className='Content'>
-          <image src={arrow} className='Arrow' />
-          <text className='Description'>Tap the logo and have fun!</text>
-          <text className='Hint'>
-            Edit<text style={{ fontStyle: 'italic' }}>{' src/App.tsx '}</text>
+        <view className="Content">
+          <image src={arrow} className="Arrow"/>
+          <text className="Description">Tap the logo and have fun!</text>
+          <text className="Hint">
+            Edit
+            <text style={{ fontStyle: "italic" }}>{" src/App.tsx "}</text>
             to see updates!
           </text>
         </view>
         <view style={{ flex: 1 }}></view>
       </view>
     </view>
-  )
+  );
 }
