00cff0e579b854e767a7d14e81c5b093277e5062
lile <lile@knowbox.cn>
2025-04-15 11:51:40 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import {
 } from "@lynx-js/react";
 
 import "./App.css";
-import "./index.css";
 import arrow from "./assets/arrow.png";
 import lynxLogo from "./assets/lynx-logo.png";
 import reactLynxLogo from "./assets/react-logo.png";
@@ -16,29 +15,40 @@ import { MainThread } from "@lynx-js/types";
 
 export function App() {
   const [alterLogo, setAlterLogo] = useState(false);
+  const [inputValue, setInputValue] = useState("");
   const [data, setData] = useState(String);
-  const logoRef = useMainThreadRef<MainThread.Element>(null);
+  const logoRef = useMainThreadRef<MainThread.Element>();
 
   useEffect(() => {
     console.info("Hello, ReactLynx");
   }, []);
 
   const handleTap = () => {
-    "main thread";
-    runOnBackground(setAlterLogo)(!alterLogo);
+    console.log("handle bind tap 112233");
+    // "main thread";
+    // runOnBackground(setAlterLogo)(!alterLogo);
   };
 
   const onTap = useCallback(() => {
     "main thread";
     runOnBackground(setAlterLogo)(!alterLogo);
-
-    console.log("logoRef : " + logoRef.current);
   }, [alterLogo]);
 
   useLynxGlobalEventListener("testEvent", (data) => {
     console.log("testEvent : " + data);
     setAlterLogo(!alterLogo);
   });
+
+  // 接收input回调
+  useLynxGlobalEventListener("myLynxInput", (data) => {
+    console.log("myLynxInput : " + data);
+  });
+
+  const handleInput = (e) => {
+    const currentValue = e.detail.value.trim();
+    setInputValue(currentValue);
+    console.log("setInputValue : " + inputValue);
+  };
 
   return (
     <view>
@@ -52,9 +62,25 @@ export function App() {
               : <image src={lynxLogo} className="Logo--lynx"/>}
           </view>
           {alterLogo ? <text className="Title">Test Msg</text> :
-            <text className="Title">MsgHaha</text>}
-          <input className="myInput" type={"text"} />
-          <text className="Title">React</text>
+            <text className="Title">MsgHaha1</text>}
+          <view className={"inputContainer"}>
+
+            <input className="myInput" placeholder={"test"}
+                   style={{
+                     color: "red",
+                     backgroundColor: "lightgray",
+                     fontSize: "40px"
+                   }}
+              // style={{ color: "red" }}
+              // style={{ backgroundColor: "yellow" }}
+                   bindinput={(e) => {
+                     console.log("input : " + e.detail.value);
+                   }}/>
+
+            {/*<hhinput className="myInput" bindinput={handleInput}*/}
+            {/*       value={inputValue}/>;*/}
+          </view>
+          <text className="Title" bindtap={handleTap}>Lynx Game</text>
           <text className="Subtitle">on Lynx</text>
         </view>
         <view className="Content">
